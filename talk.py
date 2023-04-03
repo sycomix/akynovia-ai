9de3139446527d61019b8265831ce9e2aaa328bb
Eduardo Soares <eduardo.morow.184@gmail.com>
2023-04-03 14:26:04 -0300

    Create talk.py

---
+++ talk.py
@@ -0,0 +1,7 @@
+def talk():
+    print("Digite 'sair' para encerrar a conversa.")
+    while True:
+        message = input("Você: ")
+        if message == "sair":
+            break
+        print("Akynovia: Desculpe, ainda não fui programada para conversar. Tente novamente mais tarde.")
