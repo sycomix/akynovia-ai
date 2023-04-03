da5d5e3ee03d109f0c4343799deb299d25c52599
Eduardo Soares <eduardo.morow.184@gmail.com>
2023-04-03 15:03:45 -0300

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo Instalando dependências do Python...
+pip install -r requirements.txt
+
+echo Instalando dependências do sistema...
+REM Adicione aqui os comandos para instalar as dependências do sistema
+REM Exemplo: chocolatey install w3m
+
+echo Instalação concluída!
+pause
